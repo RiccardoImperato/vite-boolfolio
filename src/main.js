d04d8e980358ae3943d38e968240b1df62f85872
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-15 15:37:45 +0100

    feat: bootstrap config

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import '../src/assets/scss/style.scss'
-import App from './App.vue'
+import { createApp } from 'vue';
+import '../src/scss/style.scss';
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
