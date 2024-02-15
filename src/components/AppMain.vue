9766a6a3b0c32b1163b93cc459957d7678736476
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-15 17:04:05 +0100

    feat: componente card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,10 @@
 <script >
 import ProjectCard from './ProjectCard.vue';
 export default {
-    name: 'Main',
     props: ['projects'],
-
+    components: {
+        ProjectCard
+    }
 };
 </script>
 
@@ -12,16 +13,7 @@ export default {
         <div class="container">
             <h2>Progetti</h2>
             <dvi class="row">
-                <div class="col col-md-4 g-4" v-for="project in projects">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ project.title }}</h5>
-                            <p class="card-text">
-                                {{ project.description }}
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                <ProjectCard v-for="project in projects" :title="project.title" :description="project.description" />
             </dvi>
         </div>
     </main>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,24 @@
+<script >
+export default {
+    props: {
+        title: String,
+        description: String
+    },
+
+};
+</script>
+
+<template>
+    <div class="col col-md-4 g-4">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">{{ title }}</h4>
+                <p class="card-text">
+                    {{ description }}
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
