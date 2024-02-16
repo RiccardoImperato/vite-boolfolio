bba2bb43124f9bd3b25587b26e6783f0bc57ecbe
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-16 16:52:33 +0100

    fix: stile link navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,9 +8,9 @@ export default {
     <header>
         <nav class="py-3">
             <ul class="list-unstyled d-flex gap-5 justify-content-center align-items-center m-0">
-                <li><router-link :to="{ name: 'home' }">Home</router-link></li>
-                <li><router-link :to="{ name: 'projects' }">Progetti</router-link></li>
-                <li><router-link :to="{ name: 'about' }">Contatti</router-link></li>
+                <li><router-link :to="{ name: 'home' }" class="link">Home</router-link></li>
+                <li><router-link :to="{ name: 'projects' }" class="link">Progetti</router-link></li>
+                <li><router-link :to="{ name: 'about' }" class="link">Contatti</router-link></li>
             </ul>
         </nav>
     </header>
@@ -20,5 +20,10 @@ export default {
 header {
     background-color: #222222;
     color: #ffff;
+
+    .link {
+        text-decoration: none;
+        color: inherit;
+    }
 }
 </style>
