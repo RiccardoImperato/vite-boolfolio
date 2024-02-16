3423d6d1c4ed59b8f7d6b4eef23b7618acbaf7b2
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-16 15:41:15 +0100

    feat: router-link menu di navigazione

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,9 +8,9 @@ export default {
     <header>
         <nav class="py-3">
             <ul class="list-unstyled d-flex gap-5 justify-content-center align-items-center m-0">
-                <li>Home</li>
-                <li>Progetti</li>
-                <li>Contatti</li>
+                <li><router-link :to="{ name: 'home' }">Home</router-link></li>
+                <li><router-link :to="{ name: 'projects' }">Progetti</router-link></li>
+                <li><router-link :to="{ name: 'about' }">Contatti</router-link></li>
             </ul>
         </nav>
     </header>
