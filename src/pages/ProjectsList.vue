01cc6ae6f6406dbe3887f8fa490f761917484a1e
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-16 16:44:32 +0100

    feat: paginazione risultati

--- src/pages/ProjectsList.vue
+++ src/pages/ProjectsList.vue
@@ -6,6 +6,8 @@ export default {
     name: 'ProjectsList',
     data() {
         return {
+            currentPage: 1,
+            responseData: {},
             'projects': [],
             'baseUrl': 'http://127.0.0.1:8000/',
             'apiUrls': {
@@ -18,12 +20,24 @@ export default {
     },
     methods: {
         getProjects() {
-            axios.get(this.baseUrl + this.apiUrls.projects).then(response => {
-                this.projects = response.data.results;
+            axios.get(this.baseUrl + this.apiUrls.projects, {
+                params: {
+                    page: this.currentPage
+                }
+            }).then(response => {
+                this.responseData = response.data
             }).catch((error) => {
                 console.log(error);
             });
-        }
+        },
+        nextPage() {
+            this.currentPage++;
+            this.getProjects();
+        },
+        prevPage() {
+            this.currentPage--;
+            this.getProjects();
+        },
     },
     created() {
         this.getProjects();
@@ -36,8 +50,21 @@ export default {
         <div class="container">
             <h2 class="mt-4">Progetti</h2>
             <div class="row">
-                <ProjectCard v-for="project in projects" :title="project.title" :description="project.description" />
+                <ProjectCard v-for="project in responseData.results?.data" :title="project.title"
+                    :description="project.description" />
             </div>
+            <nav class="my-5">
+                <ul class="d-flex justify-content-center gap-4 list-unstyled">
+                    <li>
+                        <button class="btn btn-primary" @click="prevPage"
+                            v-show="responseData.results?.prev_page_url">prev</button>
+                    </li>
+                    <li>
+                        <button class="btn btn-primary" @click="nextPage"
+                            v-show="responseData.results?.next_page_url">next</button>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </main>
 </template>
