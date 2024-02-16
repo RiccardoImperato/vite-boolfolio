bd505620c197c88f86d237d89c400b3dcf0e1c72
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-16 15:22:48 +0100

    feat: componente header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<template>
+    <header>
+        <nav class="py-3">
+            <ul class="list-unstyled d-flex gap-5 justify-content-center align-items-center m-0">
+                <li>Home</li>
+                <li>Progetti</li>
+                <li>Contatti</li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped lang="scss">
+header {
+    background-color: #222222;
+    color: #ffff;
+}
+</style>
