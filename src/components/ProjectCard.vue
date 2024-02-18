73bd19cb46330285b3191859caa498fd7a48f3f7
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-18 19:36:10 +0100

    feat: link per dettaglio

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -3,7 +3,8 @@ export default {
     name: 'ProjectCard',
     props: {
         title: String,
-        description: String
+        description: String,
+        slug: String,
     },
 
 };
@@ -12,11 +13,13 @@ export default {
 <template>
     <div class="col col-md-4 g-4">
         <div class="card">
-            <div class="card-body">
-                <h4 class="card-title">{{ title }}</h4>
-                <p class="card-text">
-                    {{ description }}
-                </p>
+            <div class="card-body d-flex flex-column justify-content-between align-items-start">
+                <div>
+                    <h4 class="card-title mb-3">{{ title }}</h4>
+                    <p class="card-text">{{ description }}</p>
+                </div>
+                <router-link :to="{ name: 'single-project', params: { slug: slug } }"
+                    class="btn btn-primary">Dettagli</router-link>
             </div>
         </div>
     </div>
@@ -24,6 +27,10 @@ export default {
 
 <style scoped lang="scss">
 .card {
-    height: 200px;
+    height: 250px;
+
+    .detail {
+        margin-top: auto;
+    }
 }
 </style>
