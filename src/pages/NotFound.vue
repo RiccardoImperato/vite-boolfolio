4141314b645b229ffdd56de57dd807b34bb2abcb
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-18 19:35:21 +0100

    feat:mpagina not found

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: 'NotFound'
+}
+</script>
+
+<template>
+    <div class="container text-center py-5">
+        <h3>Pagina non trovata</h3>
+        <p><router-link :to="{ name: 'home' }" class="link">Torna alla Home</router-link></p>
+    </div>
+</template>
