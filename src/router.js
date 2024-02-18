0ad12184767a28721b9cf7c1cb1535afccb80dbf
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-18 19:34:46 +0100

    feat: rotta pagine single-project e not found

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AppHome from './pages/AppHome.vue';
 import ProjectsList from './pages/ProjectsList.vue';
 import AppAbout from './pages/AppAbout.vue';
+import NotFound from './pages/NotFound.vue';
+import ProjectDetail from './pages/ProjectDetail.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -18,9 +20,19 @@ const router = createRouter({
             component: ProjectsList
         },
         {
+            path: '/blog/:slug',
+            name: 'single-project',
+            component: ProjectDetail
+        },
+        {
             path: '/about',
             name: 'about',
             component: AppAbout
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'NotFound',
+            component: NotFound
         },
     ]
 });
